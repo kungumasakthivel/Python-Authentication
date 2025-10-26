e6e82c78f766b011435b038765af1a583e0f32ce
kungumasakthivel <kungumasakthivel.k@gmail.com>
2025-10-26 22:56:34 +0530

    done db modelling db class and db instance

---
+++ main.py
@@ -0,0 +1,72 @@
+from flask import Flask, render_template, request, redirect, session, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+app.secret_key = os.getenv("SECRET_KEY")
+
+# Config SQL Alchemy
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONs"] = False
+db = SQLAlchemy(app)
+
+# Database Model
+class User(db.Model):
+    # class variable
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(25), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+#Routes
+@app.route("/")
+def home():
+    if "username" in session:
+        return redirect(url_for('dashboard'))
+    return render_template("index.html")
+
+
+# Login
+@app.route("/login", methods=["POST"])
+def login():
+    # Collect info from the form check if its in db otherwise show home page
+    username = request.form['username']
+    password = request.form['password']
+    user = User.query.filter_by(username=username).first()
+    if user and user.check_password(password):
+        session['username'] = username
+        return redirect(url_for('dashboard'))
+    else:
+        return render_template("index.html")
+
+
+# Register
+
+
+
+
+
+# Dashboard
+
+
+
+
+
+#Logout
+
+
+
+
+
+
+if __name__ in "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
